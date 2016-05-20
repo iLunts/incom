a266f76fbc1980c440b4236cfb632784b2220130
iLunts <shipik2008@gmail.com>
2016-05-20 14:49:58 +0200

    Fixed filter panel, add button__group element

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,7 +1,9 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute', 'ui.bootstrap']);
 
 app.controller("homeController", function ($scope) {
+});
 
+app.controller("flatController", function ($scope) {
 });
 
 app.config(function($routeProvider){
@@ -15,5 +17,12 @@ app.config(function($routeProvider){
             templateUrl:'views/nedvizhimost.html',
             controller:'nedvizhimostController'
         });
+
+        $routeProvider.when('/sale-realty/flats',
+        {
+            templateUrl:'views/sale-realty/flats.html',
+            controller:'flatController'
+        });
+
         $routeProvider.otherwise({redirectTo: '/'});
 });
